53de9768095acbf0852e54cc3d398a6dd1a2c77d
Sam Carbine <scarbine@me.com>
2021-05-28 14:48:10 -0500

    inital commit

---
+++ shopping.js
@@ -0,0 +1,40 @@
+
+const shoppingList = [
+    {
+        id: 1,
+        name: "milk",
+        price: 3.25
+    },
+    {
+        id: 2,
+        name: "Eggs",
+        price: 4.75
+    },
+    {
+        id: 3,
+        name: "Pizza Rolls",
+        price: 5.99
+    },
+    {
+        id: 4,
+        name: "Nut Butter",
+        price: 1.99
+    },
+    {
+        id: 5,
+        name: "Blueberries",
+        price: 3.49
+    },
+    {
+        id: 6,
+        name: "Beyond Burgers",
+        price: 5.99
+    },
+    {
+        id: 7,
+        name: "Carrots",
+        price: 1.99
+    },
+]
+
+console.log(shoppingList)
